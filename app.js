8269319ee066e47b86f53467000a21d85177f237
JoeKaleli <kalelimburu9@gmail.com>
2023-12-11 10:18:26 +0300

    Finalized the assignment

---
+++ app.js
@@ -0,0 +1,22 @@
+/* let condition ="Sunny"
+
+if (condition=="Rainy") {
+    console.log("Carry an umbrella")
+} else if (condition == "SUnny") {
+    console.log("Carry your shades")
+} else {
+    console.log("It's a chill day")
+
+ let function accessHere(condition) {
+    if (condition=="Joe"){
+        console.log("Get in")
+    }else if(condition=="Kaleli") {
+        console.log("Get in")
+    }else {
+        console.log("Access denied")
+    }
+}
+
+accessHere("Kaleli")
+console.log("Kaleli")*/
+
